361afb11e40a4fdb2beb2f7ee647cc96dd0706c0
dungnnb <dungnn1105@gmail.com>
2020-08-15 14:28:18 +0700

    Fist commit

---
+++ app/src/Component/Header.js
@@ -0,0 +1,59 @@
+import React,{Component} from 'react';
+import { connect } from 'react-redux';
+import * as action from './../Redux/actions/index';
+
+class Header extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            textinput: '',
+        }
+    }
+
+    handleChange = (even) => {
+        const text = even.target.value;
+        this.setState({
+            textinput: text,
+        })
+    }
+
+    clearInput = () => {
+        this.setState({
+            textinput: '',
+        })
+    }
+
+    handleList = () => {
+        const {textinput} = this.state;
+        const {onAddlish} = this.props;
+        if(textinput === ''){
+            alert("Hãy nhập công việc");
+            this.textinput.focus();
+        }else {
+            onAddlish(textinput);
+            this.setState({
+                textinput: '',
+            })
+        }
+    }
+
+    render(){
+        const {textinput} = this.state;
+        return (
+            <div  className='Header'>
+                <input style={{margin: '20px'}}  ref={(input) => this.textinput = input} value={textinput} onChange={this.handleChange}/>
+                <button style={{margin: '20px'}} onClick={this.handleList}>Thêm Công Việc Mới</button>
+            </div>
+        )
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        onAddlish: (textInput) => {
+            dispatch(action.addlist(textInput));
+        }
+    }
+}
+
+export default connect(null, mapDispatchToProps, null, {forwardRef: true})(Header);
---
+++ app/src/Component/dyung.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright 2016-present, Bkav, Corp.
+ * All rights reserved.
+ *
+ * This source code is licensed under the Bkav license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
+ * @author DungNNb@bkav.com on 30/07/2020
+ *
+ * History:
+ * @modifier abc@bkav.com on xx/xx/xxxx đã chỉnh sửa abcxyx (Chỉ các thay đổi quan trọng mới cần ghi lại note này)
+ */
